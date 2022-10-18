bf09019943c3faff7a2a10b0cc41f1024685fde2
Frodriks <frodriks@gmail.com>
2022-10-18 11:56:16 +0200

    HelloWorld5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,18 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!"); // Hallå eller!
+		// System.out.println("So you're " + age + " years old");
 		
+		String firstName = "Fredrik ";
+		String lastName = "Ohlander";
+		String fullName = firstName + lastName;
+		System.out.println(fullName);
+		
+		int x = 15;
+		int y = 14;
+		int z = x + y;
+		System.out.println("My age is " + z);
 	}
 
 }
