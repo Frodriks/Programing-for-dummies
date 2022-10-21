ccc15a118274b4ada4fd9eb9ed62002ad42a4ebf
Frodriks <frodriks@gmail.com>
2022-10-21 11:51:23 +0200

    HelloWorld10

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,47 @@
 
 public class HelloWorld {
-
+	static void myMethod() { //Parameters and Arguments, single Parameter
+		System.out.println("It's friday!!!!!\n");
+	}
+	static void yourMethod(String fname, int age) { //Parameters and Arguments, Multiple Parameters
+		System.out.println(fname + " TORM" + " is " + age);
+	}
+	static int theirMethod(int q, int w) { //Parameters and Arguments, Return Values
+		return q + w;
+	}
+	static void checkAge(int oldEnough) {
+		if (oldEnough < 18) {
+			System.out.println("Access denied!!! You're not old enough!!");
+		}
+		else {
+			System.out.println("Access granted!! You're old enough!!");
+		}
+	}
+	static int plusMethod(int a, int s) { //Method Overloading, int
+		return a + s;
+	}
+	static double plusMethod(double a, double s) { //Method Overloading , double
+		return a + s;
+	}
+	
 	public static void main(String[] args) {
-		String txt = "Hello World!\n";
+		myMethod();
+		yourMethod("Fredrik", 29);
+		yourMethod("Frida", 28);
+		yourMethod("Freja", 10);
+		yourMethod("Malin", 28);
+		int e = theirMethod(5, 3); //Parameters and Arguments, Return Values
+		System.out.println("\n" + e + "\n"); //Parameters and Arguments, Return Values
+		checkAge(20);
+		checkAge(17);
+		int myNum1 = plusMethod(8, 5); //Method Overloading, int
+		double myNum2 = plusMethod(3.33, 6.66); //Method Overloading , double
+		System.out.println("\nint: " + myNum1); //Method Overloading, int
+		System.out.println("double: " + myNum2); //Method Overloading , double
+	
+	
+		
+		String txt = "\nHello World!\n";
 		System.out.println(txt.toUpperCase()); // Hallå eller!
 		// System.out.println("So you're " + age + " years old");
 		
@@ -47,37 +86,84 @@ public class HelloWorld {
 		System.out.print("\nThe day of the week is:" + " ");
 		switch (day) {
 		case 1:
-			System.out.println("Monday!");
+			System.out.println("Monday!\n");
 			break;
 		case 2:
-			System.out.println("Tuesday!");
+			System.out.println("Tuesday!\n");
 			break;
 		case 3:
-			System.out.println("Wednesday!");
+			System.out.println("Wednesday!n");
 			break;
 		case 4:
-			System.out.println("Thursday!");
+			System.out.println("Thursday!n");
 			break;
 		case 5:
-			System.out.println("Friday!");
+			System.out.println("Friday!\n");
 			break;
 		case 6:
-			System.out.println("Saturday!");
+			System.out.println("Saturday!\n");
 			break;
 		case 7:
-			System.out.println("Sunday!");
+			System.out.println("Sunday!\n");
 			break;
 		default: //If the "Math.Random" is "0", this will be printed out
-			System.out.println("ORIMLIGT!");
+			System.out.println("ORIMLIGT!\n");
 		}
 		
-		int i = 0; 
+		//do-while loop
+		int i = 0; //Starts counting from 0
 		while (i<=5) { //Counts up top 5, then it stops
-			System.out.println ("\n" + i);
+			System.out.println (i);
 			i++; //Adds +1 to the counting
 		}
+		int o = 1; //starts counting from 1
+		do {
+			System.out.println(o);
+			o++;
+		}	
+		while (o < 6); //Stops counting at 5 because it prints as long as it's less than 6
+		
+		//For Loop -When you know exactly how many times you want to loop through a block of code, use the for loop instead of a while loop:
+		for (int k = 0; k <= 10; k = k + 2) {
+			System.out.println("Yes");
+			
+		}
+		
+		//For-Each Loop - There is also a "for-each" loop, which is used exclusively to loop through elements in an array
+		String[] cars = {"Volvo", "BMW", "SAAB", "Wolksvagen"};
+		for (String l : cars) {
+			System.out.println(l);
+		}
+		
+		//Java Break
+		for (int m = 1; m < 10; m++) {
+			if (m == 5) {
+				break;
+			}
+			System.out.println(m);
+		}
+		
+		//Java Continue
+		for (int n = 1; n <= 10; n++) {
+			if (n == 5) {
+				continue;
+			}
+			System.out.println(n);
+		}
+		
+		//Java methods
+		//int goal = sum(10); //Java Recursion
+		//System.out.println(goal);
+		//public static int sum(int b) {
+			//if(b >0) {
+				//return b + sum(b - 1);
+			//}
+			//else
+			//return 0;
+		//}
 		
 		
+		myMethod();
 	}
 
 }
